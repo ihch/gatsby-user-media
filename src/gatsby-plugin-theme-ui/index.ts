af30785f170eaa0bfff9cfd19879f3983815c195
ihch <ryubi.102395@gmail.com>
2020-12-26 22:30:47 +0900

    feat: styleの定義をしてみた

---
+++ src/gatsby-plugin-theme-ui/index.ts
@@ -0,0 +1,19 @@
+import { Theme } from 'theme-ui';
+
+const theme: Theme = {
+  colors: {
+    text: '#555',
+    background: '#eee',
+  },
+  styles: {
+    div: {
+      backgroundColor: 'background',
+    },
+    h1: {
+      color: 'text',
+      backgroundColor: '#eee',
+    },
+  },
+};
+
+export default theme;
